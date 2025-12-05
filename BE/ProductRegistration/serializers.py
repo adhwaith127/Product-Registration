4a4bce27c021c613d45566f02fe6c30d301c6318
adhwaith127 <219108345+adhwaith127@users.noreply.github.com>
2025-12-05 15:25:27 +0530

    serializers

--- BE/ProductRegistration/serializers.py
+++ BE/ProductRegistration/serializers.py
@@ -9,7 +9,8 @@ class DeviceSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_serialnumber(self,value):
+        
         pattern = "^[0-9]{4}[0-9]{2}(AMP|API)[0-9]{6}B$"
 
-        if re.fullmatch(pattern=pattern, string=value):
-            
+        if not re.fullmatch(pattern=pattern, string=value):
+            serializers.ValidationError("Serial number not following pattern")
