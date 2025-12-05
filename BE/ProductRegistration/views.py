cc70bdf0ed1799da26bfd0b6968e3b623f078a4a
adhwaith127 <219108345+adhwaith127@users.noreply.github.com>
2025-12-05 16:32:56 +0530

    handle incoming requests

--- BE/ProductRegistration/views.py
+++ BE/ProductRegistration/views.py
@@ -6,14 +6,15 @@ from .models import Serialdata,PalmtecUpiDetails
 from .serializers import DeviceSerializer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def add_serial_number(request):
     try:
         serializer = DeviceSerializer(data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
-            return Response({"Success": "Serial Number Added", "data": serializer.data}, status=status.HTTP_201_CREATED)
-    
+                serializer.save()
+                return Response({"message": "success", "data": serializer.data})
+        return Response({"message": "error", "errors": serializer.errors}, status=400)
+
     except Exception as e:
         return Response({"Error":f"Error in received data,{str(e)}","status":status.HTTP_400_BAD_REQUEST})
