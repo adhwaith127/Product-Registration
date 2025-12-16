d1278d2099dcc2b9ea57c50867f7af3709584111
adhwaith127 <219108345+adhwaith127@users.noreply.github.com>
2025-12-16 17:43:37 +0530

    new urls for mapping devices to customers

--- BE/ProductRegistration/urls.py
+++ BE/ProductRegistration/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('allocate_serial_number/', views.allocate_serial_number, name="allocate_serial_number"),
     path('getSerialNumber/', views.get_unallocated_sl_no, name="get_unallocated_sl_no"),
     path('get_customer_mappings/', views.get_customer_mappings, name="get_customer_mappings"),
+    path('create_customer_mapping/', views.create_customer_mapping, name="create_customer_mapping"),
+    path('update_customer_mapping/', views.update_customer_mapping, name="update_customer_mapping"),
+    path('get_device_details/', views.get_device_details, name="get_device_details"),
 ]
